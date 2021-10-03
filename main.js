da95423a2524cb2e9ef53838fd67431b97ccdbed
Minh Quan <minkkquan2@gmail.com>
2021-10-03 17:54:26 +0700

    update editable, save task

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var btn = document.querySelector('.add-btn');
 var input = document.querySelector('.input-job');
 var list = document.querySelector('.job-list');
+var saveBtn = document.querySelector('.save-btn');
+
 var pos;
+var doneTask = [];
 var storageKey = 'todoList';
 var data = localStorage.getItem(storageKey);
 var todoList;
@@ -28,6 +31,7 @@ document.addEventListener('keypress', function(e){
     switch(e.key){
         case 'Enter':
             addJob();
+            update();
             break;
     }
 })
@@ -36,12 +40,18 @@ btn.addEventListener('click', addJob);
 function render(){
     // list.innerHTML += `<li>${job}</li>`;
     var content = todoList.map(function(job, index){
-        return `<li class="job-item"><span>${job}</span>`+`<i class ="ti-close close-btn" onclick="deleteJob(${index})"></i>`+`</li>`;
+        return `<li class="job-item" contenteditable="true"><span>${job}</span>`+`<i class ="ti-close close-btn" onclick="deleteJob(${index})"></i>`+`</li>`;
     })
     list.innerHTML = content.join('');
 }
 render();
-
+function update(){
+    var jobItemList = document.querySelectorAll('.job-item');
+    for(i = 0; i < jobItemList.length; i++){
+        todoList[i] = jobItemList[i].innerText;
+    }
+    localStorage.setItem(storageKey, JSON.stringify(todoList));
+}
 function deleteJob(pos){
     job = todoList[pos];
     // e.target.parentNode.remove();
@@ -52,3 +62,4 @@ function deleteJob(pos){
     render();
     // addClose();
 }
+saveBtn.addEventListener('click', update);
