6c2f46a43543c26a5615e6f6a2b14b9f7b03b9fe
Minh Quan <minkkquan2@gmail.com>
2021-10-07 22:23:15 +0700

    add done task func, small fixes

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ var saveBtn = document.querySelector('.save-btn');
 
 var pos;
 var doneTask = [];
+var doneJS = localStorage.getItem('doneTask');
+if (doneJS){
+    doneTask = JSON.parse(doneJS);
+} else {
+    doneTask = [];
+}
 var storageKey = 'todoList';
 var data = localStorage.getItem(storageKey);
 var todoList;
@@ -40,9 +46,14 @@ btn.addEventListener('click', addJob);
 function render(){
     // list.innerHTML += `<li>${job}</li>`;
     var content = todoList.map(function(job, index){
-        return `<li class="job-item" contenteditable="true"><span>${job}</span>`+`<i class ="ti-close close-btn" onclick="deleteJob(${index})"></i>`+`</li>`;
+        if(doneTask.includes(job)){
+            return `<li class="job-item" contenteditable="false"><span class="job-name job-done">${job}</span>`+`<i class ="ti-check check-btn" onclick="doneJob(${index})"></i>`+`<i class ="ti-close close-btn" onclick="deleteJob(${index})"></i>`+`</li>`
+        } else {
+            return `<li class="job-item" contenteditable="true"><span class="job-name">${job}</span>`+`<i class ="ti-check check-btn" onclick="doneJob(${index})"></i>`+`<i class ="ti-close close-btn" onclick="deleteJob(${index})"></i>`+`</li>`;
+        }
     })
     list.innerHTML = content.join('');
+
 }
 render();
 function update(){
@@ -57,9 +68,24 @@ function deleteJob(pos){
     // e.target.parentNode.remove();
     pos = todoList.indexOf(job);
     todoList.splice(pos,1);
+    if(doneTask.includes(job)){
+        pos = doneTask.indexOf(job);
+        doneTask.splice(pos,1);
+        localStorage.setItem('doneTask',JSON.stringify(doneTask));
+    }
     localStorage.setItem(storageKey, JSON.stringify(todoList));
     todoList = JSON.parse(localStorage.getItem(storageKey));
     render();
     // addClose();
 }
 saveBtn.addEventListener('click', update);
+
+function doneJob(pos){
+    job = todoList[pos];
+    if(doneTask.includes(job)){
+    } else {
+        doneTask.push(job);
+        localStorage.setItem('doneTask', JSON.stringify(doneTask));
+        render();
+    }
+}
